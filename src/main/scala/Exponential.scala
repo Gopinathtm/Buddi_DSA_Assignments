471a96ac31ca9564db024904b5c5208adbebd5a7
Gopi <gopinathtmofficial@gmail.com>
2022-11-07 20:20:58 +0530

    All methods got same answer automated

--- src/main/scala/Exponential.scala
+++ src/main/scala/Exponential.scala
@@ -1,6 +1,8 @@
+import scala.collection.mutable.ArrayBuffer
+
 object Exponential {
 
-  def power(base: Long, pow: Int): Double = {
+  def power(base: Double, pow: Int): Double = {
     var ans: Double = 1.0
     (1 to pow).foreach { _ =>
       ans *= base
@@ -16,18 +18,18 @@ object Exponential {
     ans
   }
 
-  def exponential_methodOne(x: Long): Double = {
+  def exponential_methodOne(x: Double): Double = {
     var ans: Double = 1
-    (1 to 100).foreach { i =>
+    (1 to 30).foreach { i =>
       ans += power(x, i) / factorial(i)
     }
     ans
   }
 
-  def exponential_methodTwo(x: Int): Double = {
+  def exponential_methodTwo(x: Double): Double = {
     var ans: Double = 1
     var prev: Double = 1
-    (1 to 700).foreach { i =>
+    (1 to 200).foreach { i =>
       prev = prev * x / i
       ans += prev
     }
@@ -35,21 +37,33 @@ object Exponential {
   }
 
   def exponential_methodThree(x: Double): Double = {
-    var prev: Double = 1
+    var prev: Double = 1.0
     (700 to 1 by -1).foreach { i =>
       prev = 1 + (x / i) * prev
     }
     prev
   }
 
-  def main(args: Array[String]): Unit = {
-    val n = 29
-    println(s"Naive method ${exponential_methodOne(n)}")
-    println(s"Standard library ${math.exp(n)}")
-    println(s"Method Two ${exponential_methodTwo(n)}")
-    println(s"Method Three ${exponential_methodThree(n)}")
-
-
+  def timeTaken(method: Double => Double, n: Double, methodType: String): Long = {
+    val start = System.currentTimeMillis()
+    println(s"$methodType started  for n = $n")
+    (0 to 1000000).foreach(_ => method(n))
+    val end = System.currentTimeMillis()
+    println(s"$methodType ended for n = $n is ${end - start}ms and value = ${method(n)}")
+    end - start
   }
+
+  def main(args: Array[String]): Unit = {
+    val inputList: List[Double] = List(5.0, 11.0, 17.0, 23.0, 29.0)
+    val methodList = List("Library method", "O(n) first method", "O(n) second method", "Naive method")
+    val methods: List[Double => Double] = List(math.exp, exponential_methodThree, exponential_methodTwo, exponential_methodOne)
+    val result: ArrayBuffer[Long] = ArrayBuffer()
+
+    val method_wise_tuple = methods zip methodList
+    inputList.foreach{n =>
+      method_wise_tuple.foreach{ methodTuple => result.append(timeTaken(methodTuple._1, n, methodTuple._2))}
+    }
+    println(result)
+    }
 
 }
