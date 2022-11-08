9f46e32712f095a64185a15f9a17b9fa4bb32a05
gopinathtm <gopinathtm@buddi.ai>
2022-11-08 14:47:43 +0530

    x value changed to 2 and code modified

--- src/main/scala/Exponential.scala
+++ src/main/scala/Exponential.scala
@@ -2,16 +2,16 @@ import scala.collection.mutable.ArrayBuffer
 
 object Exponential {
 
-  def power(base: Double, pow: Int): Double = {
-    var ans: Double = 1.0
+  def power(base: Int, pow: Int): Long = {
+    var ans: Long = 1
     (1 to pow).foreach { _ =>
       ans *= base
     }
     ans
   }
 
-  def factorial(factOf: Long): Double = {
-    var ans: Double = 1.0
+  def factorial(factOf: Long): Long = {
+    var ans: Long = 1
     (factOf until 1 by -1).foreach { i =>
       ans *= i
     }
@@ -20,8 +20,8 @@ object Exponential {
 
   def exponential_methodOne(x: Double): Double = {
     var ans: Double = 1
-    (1 to 20).foreach { i =>
-      ans += power(x, i) / factorial(i)
+    (1 to x.toInt).foreach { i =>
+      ans += power(2, i) / factorial(i)
     }
     ans
   }
@@ -29,8 +29,8 @@ object Exponential {
   def exponential_methodTwo(x: Double): Double = {
     var ans: Double = 1
     var prev: Double = 1
-    (1 to 200).foreach { i =>
-      prev = prev * x / i
+    (1 to x.toInt).foreach { i =>
+      prev = prev * 2 / i
       ans += prev
     }
     ans
@@ -38,8 +38,8 @@ object Exponential {
 
   def exponential_methodThree(x: Double): Double = {
     var prev: Double = 1.0
-    (200 to 1 by -1).foreach { i =>
-      prev = 1 + (x / i) * prev
+    (x.toInt to 1 by -1).foreach { i =>
+      prev = 1 + (2 / i) * prev
     }
     prev
   }
@@ -47,14 +47,14 @@ object Exponential {
   def timeTaken(method: Double => Double, n: Double, methodType: String): Long = {
     val start = System.currentTimeMillis()
 //    println(s"$methodType started  for n = $n")
-    (0 to 1000000).foreach(_ => method(n))
+    (0 to 1000000).foreach(_ => method(2))
     val end = System.currentTimeMillis()
-    println(s"$methodType ended for n = $n is ${end - start}ms and value = ${method(n)}")
+    println(s"$methodType ended for n = $n is ${end - start}ms and value = ${method(2)}")
     end - start
   }
 
   def main(args: Array[String]): Unit = {
-    val inputList: List[Double] = List(5.0, 11.0, 17.0, 23.0, 29.0)
+    val inputList: List[Double] = List(1.0, 5.0, 11.0, 17.0, 23.0, 29.0)
     val methodList = List("Library method", "O(n) first method", "O(n) second method", "Naive method")
     val methods: List[Double => Double] = List(math.exp, exponential_methodTwo,exponential_methodThree, exponential_methodOne)
     val result: ArrayBuffer[Long] = ArrayBuffer()
