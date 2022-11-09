c887d36d72b472ef970b6c9db298f37678897386
Gopi <gopinathtmofficial@gmail.com>
2022-11-09 22:21:50 +0530

    exp Method modified for standard method and library method

--- src/main/scala/Exponential.scala
+++ src/main/scala/Exponential.scala
@@ -18,51 +18,64 @@ object Exponential {
     ans
   }
 
-  def exponential_methodOne(x: Double): Double = {
+  def exponential_methodOne(n: Double, x:Double): Double = {
     var ans: Double = 1
-    (1 to x.toInt).foreach { i =>
-      ans += power(2, i) / factorial(i)
+    (1 to n.toInt).foreach { i =>
+      ans += power(x.toInt, i) / factorial(i)
     }
     ans
   }
 
-  def exponential_methodTwo(x: Double): Double = {
+  def exponential_methodTwo(n: Double, x:Double): Double = {
     var ans: Double = 1
     var prev: Double = 1
-    (1 to x.toInt).foreach { i =>
-      prev = prev * 2 / i
+    (1 to n.toInt).foreach { i =>
+      prev = prev * x / i
       ans += prev
     }
     ans
   }
 
-  def exponential_methodThree(x: Double): Double = {
+  def exponential_methodThree(n: Double, x:Double): Double = {
     var prev: Double = 1.0
-    (x.toInt to 1 by -1).foreach { i =>
-      prev = 1 + (2 / i) * prev
+    (n.toInt to 1 by -1).foreach { i =>
+      prev = 1 + (x / i) * prev
     }
     prev
   }
 
-  def timeTaken(method: Double => Double, n: Double, methodType: String): Long = {
+  def timeTakenOwnMethods(method: (Double,Double) => Double, n: Double, x:Double,  methodType: String): Long = {
     val start = System.currentTimeMillis()
 //    println(s"$methodType started  for n = $n")
-    (0 to 1000000).foreach(_ => method(2))
+    (0 to 1000000).foreach(_ => method(n,x))
     val end = System.currentTimeMillis()
-    println(s"$methodType ended for n = $n is ${end - start}ms and value = ${method(2)}")
+    println(s"$methodType ended for n = $n is ${end - start}ms and value = ${method(n,x)}")
+    end - start
+  }
+
+  def timeTakenLibraryMethod(n: Double, x:Double, methodType: String = "Math.Exp "): Long = {
+    val start = System.currentTimeMillis()
+    //    println(s"$methodType started  for n = $n")
+    (0 to 1000000).foreach(_ => math.exp(x))
+    val end = System.currentTimeMillis()
+    println(s"$methodType ended for n = $n is ${end - start}ms and value = ${math.exp(x)}")
     end - start
   }
 
   def main(args: Array[String]): Unit = {
     val inputList: List[Double] = List(1.0, 5.0, 11.0, 17.0, 23.0, 29.0)
-    val methodList = List("Library method", "O(n) first method", "O(n) second method", "Naive method")
-    val methods: List[Double => Double] = List(math.exp, exponential_methodTwo,exponential_methodThree, exponential_methodOne)
+    val methodList = List("O(n) first method", "O(n) second method", "Naive method")
+    val methods: List[(Double,Double) => Double] = List(exponential_methodTwo,exponential_methodThree, exponential_methodOne)
     val result: ArrayBuffer[Long] = ArrayBuffer()
 
     val method_wise_tuple = methods zip methodList
+    val x = 2
     inputList.foreach{n =>
-      method_wise_tuple.foreach{ methodTuple => result.append(timeTaken(methodTuple._1, n, methodTuple._2))}
+      method_wise_tuple.foreach{ methodTuple =>
+        result.append(timeTakenOwnMethods(methodTuple._1, n,x, methodTuple._2))}
     }
+    inputList.foreach{n => result.append(timeTakenLibraryMethod(n,x))}
+
     println(result)
     }
 
