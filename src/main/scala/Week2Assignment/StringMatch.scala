5526552a05c1e177091d9f8a08ad5aa50120e69c
gopinathtm <gopinathtmofficial@gmail.com>
2022-11-22 14:11:04 +0530

    data for visualization regenerated

--- src/main/scala/Week2Assignment/StringMatch.scala
+++ src/main/scala/Week2Assignment/StringMatch.scala
@@ -6,13 +6,15 @@ import scala.util.Random
 import scala.collection.mutable.ListBuffer
 import com.github.tototoshi.csv._
 
+import scala.collection.mutable
+
 object StringMatch {
-  var fileString :String = ""
+  var fileString: String = ""
 
   def readTextFile(path: String): Unit = {
     try {
       val bufferedSource = Source.fromFile(path)
-      fileString = bufferedSource.getLines().mkString(" ").replaceAll("[’”—_'-+-.^:;,\"]","")
+      fileString = bufferedSource.getLines().mkString("\n ").toLowerCase()
       bufferedSource.close()
       println("File loaded")
     }
@@ -22,8 +24,8 @@ object StringMatch {
     }
   }
 
-  def getRandomWordsFromText(InputTxt: String):Unit= {
-    val wordsList = InputTxt.split(" ").map( word => word.trim())
+  def getRandomWordsFromText(InputTxt: String): Unit = {
+    val wordsList = InputTxt.split(" ").map(word => word.trim())
     val FiveLetterKeyWord = new ListBuffer[String]
     val SixLetterKeyWord = new ListBuffer[String]
     val SevenLetterKeyWord = new ListBuffer[String]
@@ -31,7 +33,7 @@ object StringMatch {
     val NineLetterKeyWord = new ListBuffer[String]
     val TenLetterKeyWord = new ListBuffer[String]
     val rand = new Random()
-    (0 until 3000).foreach { _ =>
+    (0 until 7000).foreach { _ =>
       val randomIndex = rand.nextInt(2463683)
       val keyword: String = wordsList(randomIndex)
       keyword.length match {
@@ -44,47 +46,58 @@ object StringMatch {
         case _ => ""
       }
     }
-    val keywordsList = List(FiveLetterKeyWord.take(20), SixLetterKeyWord.take(10), SevenLetterKeyWord.take(10), EightLetterKeyWord.take(10), NineLetterKeyWord.take(10), TenLetterKeyWord.take(20))
+    val keywordsList = List(FiveLetterKeyWord.take(80), SixLetterKeyWord.take(80), SevenLetterKeyWord.take(80), EightLetterKeyWord.take(80), NineLetterKeyWord.take(80), TenLetterKeyWord.take(80))
     var keywords = ""
-    keywordsList.foreach(lst => lst.foreach(i => keywords = keywords.concat( s"$i\n")))
-    val pw = new PrintWriter(new File("keywords.txt" ))
+    keywordsList.foreach(lst => lst.foreach(i => keywords = keywords.concat(s"$i\n")))
+    val pw = new PrintWriter(new File("keywords.txt"))
     pw.write(keywords)
     pw.close()
 
   }
 
-  def timeTaken(keywordsPath:String, nonExistPath:String, completeString:String): (ListBuffer[Long], ListBuffer[Long]) ={
+  def timeTaken(keywordsPath: String, completeString: String): (ListBuffer[Long], ListBuffer[Long], ListBuffer[String]) = {
     val keywordSource = Source.fromFile(keywordsPath)
     val keywordsList = keywordSource.getLines().toList
     keywordSource.close()
-    val nonExistSource = Source.fromFile(nonExistPath)
-    val nonExistWords = nonExistSource.getLines().toList
-    nonExistSource.close()
-    val AllWords = keywordsList ++ nonExistWords
-    val containsTimeTaken = new ListBuffer[Long] ()
-    val matchTimeTaken = new ListBuffer[Long] ()
-    AllWords.foreach{ eachWord =>
+    val containsTimeTaken = new ListBuffer[Long]()
+    val matchTimeTaken = new ListBuffer[Long]()
+    val keywordList = new ListBuffer[String]()
+    keywordsList.foreach { eachWord =>
       var start = System.nanoTime()
       var result = completeString.contains(eachWord)
       var end = System.nanoTime()
-      containsTimeTaken.append(end-start)
+      containsTimeTaken.append(end - start)
       start = System.nanoTime()
       result = completeString.matches(eachWord)
       end = System.nanoTime()
-      matchTimeTaken.append(end-start)
+      matchTimeTaken.append(end - start)
+      keywordList.append(eachWord)
     }
-    (containsTimeTaken, matchTimeTaken)
+    (containsTimeTaken, matchTimeTaken, keywordList)
   }
 
-  def main(args: Array[String]): Unit = {
-    readTextFile("bible.txt")
-    (0 to 30).foreach { i =>
-      val (containsTime, matchTime) = timeTaken("keywords.txt", "NonExcistantWord.txt", fileString)
-      val writer = CSVWriter.open("out.csv", append = true)
-      writer.writeAll(List(containsTime, matchTime))
-      writer.close()
-      println( i + " file written")
+  def randomStringFromCharList(length: Int): String = {
+    val chars = ('a' to 'z') ++ ('A' to 'Z') ++ ('0' to '9')
+    val sb = new mutable.StringBuilder
+    for (i <- 1 to length) {
+      val randomNum = util.Random.nextInt(chars.length)
+      sb.append(chars(randomNum))
     }
+    sb.toString
+  }
+
+
+  def main(args: Array[String]): Unit = {
+    readTextFile("samples.txt")
+    //        getRandomWordsFromText(fileString)
+    (0 to 6).foreach { experiment =>
+      val (containsTime, matchTime, keywordList) = timeTaken("keywords.txt", fileString)
+      val writer = CSVWriter.open("newOut.csv", append = true)
+      (0 to 599).foreach(idx => writer.writeRow(List(keywordList(idx).length, keywordList(idx), containsTime(idx), matchTime(idx))))
+      writer.close()
+      println(experiment + " file written")
+    }
+
   }
 
 }
